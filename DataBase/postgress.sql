72ae255afb042863bdd9c75f55627666087e009f
saikumar <159767386+saikumar-pujari@users.noreply.github.com>
2026-06-29 16:32:44 +0530

    added all search option for the postgresql

--- DataBase/postgress.sql
+++ DataBase/postgress.sql
@@ -12,6 +12,7 @@
 -- \q                      quit psql
 -- \di                     list all index
 -- \di+ <index_name>       gives a description about that index
+--\dx                      for extensions
 -- ============================================================
 -- 3-Level Architecture
 -- ============================================================
@@ -139,9 +140,156 @@ REVOKE ALL ON studentlist FROM sai; --removes all privilages
 --=================================================
 --      SEARCH IN DB
 --=================================================
+-- ts_vector:normalise the vector or only keeps the distinct words(lexemes)
+generate always as(to_tsvector('english',coalesce('title','')||''||coalesce(body,'')))stored;
+SELECT to_tsvector('english','PostgreSQL is an advanced relational database');
 
+-- ts_query 
+SELECT to_tsquery('english','database');
+SELECT to_tsquery('english','machine & learning');
+SELECT websearch_to_tsquery('english','postgresql OR django');
 
+SELECT * FROM articles WHERE to_tsvector('english',content) @@ to_tsquery('english','database'); --uses both the ts_vector and ts_query (title || ' ' || category || ' ' || content)[Multiple columns]
+SELECT * FROM articles WHERE to_tsvector('english', title || ' ' || content) @@ plainto_tsquery('english', 'postgresql database'); --can take 2 words without 
+SELECT * FROM articles WHERE to_tsvector('english', title || ' ' || content) @@ websearch_to_tsquery('english', 'postgresql database'); --can take 2 words without better to use it
+--diff between the to_tsquery and websearch_to_tsquery is websearch can find 2 words without and in it and normal to_tsquery requires & to find 2 words
+SELECT * FROM articles WHERE to_tsvector('english',content) @@ websearch_to_tsquery('english','database postgresql'); --BOTH ARE EQUAL
+SELECT * FROM articles WHERE to_tsvector('english',content) @@ to_tsquery('english','database & postgresql'); --BOTH ARE EQUAL
 
+-- ts_rank
+SELECT title,ts_rank(to_tsvector('english',content),to_tsquery('english','postgresql'),'MaxWords=10, MinWords=5') AS rank FROM articles WHERE to_tsvector('english',content)@@to_tsquery('english','postgresql') ORDER BY rank DESC;
+--ts_rank takes both ts_tsvector & ts_tsvector for its ranking and gives a rank with most revalent text 
+
+--Headline
+select title,ts_headline('english',content,to_tsquery('english','postgresql')) from articles where to_tsvector('english',content)@@to_tsquery('english
+','postgresql'); --highlightes the specifc word we are searching for!
+-- 'MaxWords=10, MinWords=5'
+
+SELECT title,
+    ts_rank(to_tsvector('english', title || ' ' || content),websearch_to_tsquery('english', 'postgresql indexing')) AS rank, 
+    ts_headline('english', content, websearch_to_tsquery('english', 'postgresql indexing')) AS preview 
+    FROM articles 
+    WHERE
+    to_tsvector('english', title || ' ' || content)
+    @@
+    websearch_to_tsquery('english', 'postgresql indexing')
+    ORDER BY rank DESC;
+
+-- SET Weights
+SELECT
+    title,
+    ts_rank(setweight(to_tsvector('english', title),'A')
+    ||
+    setweight(to_tsvector('english', content),'B'),
+    plainto_tsquery('english','postgresql')
+    ) AS rank
+    FROM articles
+    WHERE
+    (
+    setweight(to_tsvector('english', title),'A')
+    ||
+    setweight(to_tsvector('english', content),'B')
+    )
+    @@
+    plainto_tsquery('english','postgresql')
+    ORDER BY rank DESC;
+-- PRODUCTION LEVEL CODE
+
+SELECT
+    title,
+    ts_rank(
+    setweight(to_tsvector('english',title),'A')
+    ||
+    setweight(to_tsvector('english',category),'B')
+    ||
+    setweight(to_tsvector('english',content),'C'),
+    
+    plainto_tsquery('english','postgresql')
+    ) AS rank
+    FROM articles
+    WHERE
+    (
+    setweight(to_tsvector('english',title),'A')
+    ||
+    setweight(to_tsvector('english',category),'B')
+    ||
+    setweight(to_tsvector('english',content),'C')
+    )
+    @@
+    plainto_tsquery('english','postgresql')
+    ORDER BY rank DESC;
+--GIN(Generate inverted index):Generalized Inverted Index
+-- it basically works as keeps a track of all words and store them by group
+ALTER TABLE articles
+ADD COLUMN search_vector TSVECTOR
+GENERATED ALWAYS AS
+(
+    setweight(to_tsvector('english',coalesce(title,'')),'A')
+    ||
+    setweight(to_tsvector('english',coalesce(category,'')),'B')
+    ||
+    setweight(to_tsvector('english',coalesce(content,'')),'C')
+) STORED;
+-- then this
+Create index sai on articles USING GIN(search_vector);
+--then to check
+EXPLAIN SELECT * FROM articles
+WHERE search_vector @@ plainto_tsquery('postgresql');
+
+--other Search option for indexing
+CREATE INDEX idx_search ON articles USING GIST(search_vector);
+-- use GIST for specfic reasons only and its faster from insert/delete/updation
+
+-- pg-trgm
+CREATE EXTENSION pg_trgm;
+SELECT * FROM books WHERE title % 'Postgres';
+-- always remeber to keep a threshold value for the serach
+CREATE INDEX idx_books_title_trgm ON books USING GIN(title gin_trgm_ops); --with GIN we need to use this extension for larger DB indexes
+
+--=====================================================================================================================================
+-- full code for full text serach in postgresql
+
+CREATE TABLE articles(id SERIAL PRIMARY KEY,title TEXT,content TEXT);
+insert into articles(title,content)values();
+
+--step:1 create a search_vector colums
+ALTER TABLE articles
+ADD COLUMN search_vector TSVECTOR
+GENERATED ALWAYS AS (
+    to_tsvector('english',title || ' ' || content)
+) STORED;
+
+--step 2:create a index on search_vector using the GIN
+CREATE INDEX idx_articles_fts ON articles USING GIN(search_vector);
+
+--step 3:Trigram index if not exists
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX idx_articles_title_trgm ON articles USING GIN(title gin_trgm_ops);
+
+--step 4:check
+--normal postgresql
+SELECT * FROM articles WHERE search_vector @@ plainto_tsquery('english','PostgreSQL');
+--trigram typo check
+SELECT * FROM articles WHERE title % 'PostgrSQL' ORDER BY title <-> 'PostgrSQL';
+
+--full text
+SELECT
+id,
+title,
+ts_rank(search_vector, plainto_tsquery('english','Python')) AS rank
+FROM articles
+WHERE search_vector @@ plainto_tsquery('english','Python');
+--typo check only
+SELECT id,title, similarity(title,'Python') AS score FROM articles WHERE title % 'Python';
+
+-- BOTH
+SELECT id,title,
+    ts_rank(search_vector,plainto_tsquery('english', 'PostgrSQL')) AS rank,
+    similarity(title, 'PostgrSQL') AS sim
+FROM articles
+WHERE search_vector @@ plainto_tsquery('english', 'PostgrSQL')OR title % 'PostgrSQL'
+ORDER BY (ts_rank(search_vector,plainto_tsquery('english', 'PostgrSQL')) * 2) + similarity(title, 'PostgrSQL') DESC;
+--=====================================================================================================================================
 
 -- ============================================
 -- DJANGO COMMANDS (reference only, NOT SQL)
